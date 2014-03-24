2f731cbc9a8b3461be0515560693d7931641c0f6
Christoffer Rosenqvist <christoffer.rosenqvist@invativa.se>
2014-03-24 21:42:06 +0100

    Added a basic class with a function using ServiceStack.Text from Nuget.

---
+++ Utilities/General.cs
@@ -0,0 +1,9 @@
+﻿namespace InvativaNet.Core.Utilities {
+    using ServiceStack.Text;
+
+    public class General {
+        public static string JsonSerialize(object source) {
+            return JsonSerializer.SerializeToString(source);
+        }
+    }
+}
